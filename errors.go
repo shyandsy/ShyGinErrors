9a2dbbfe541af3793ac1c828919695e7136dff49
yshi3 <yshi3@tesla.com>
2021-11-15 11:17:57 +0800

    gin BindJson errors parsing

---
+++ errors.go
@@ -0,0 +1,74 @@
+package ShyGinErrors
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type GinErrors interface {
+	ListAllErrors(model interface{}, err error) map[string]string
+}
+
+type ginErrors struct {
+	errorMaps map[string]string
+}
+
+type ErrorResult struct {
+	Field   string
+	JsonTag string
+	Message string
+}
+
+func NewShyGinErrors(errors map[string]string) GinErrors {
+	return &ginErrors{
+		errorMaps: errors,
+	}
+}
+
+func (ge ginErrors) ListAllErrors(model interface{}, err error) map[string]string {
+	errors := map[string]string{}
+	fields := map[string]ErrorResult{}
+
+	// resolve all json tags for the struct
+	types := reflect.TypeOf(model)
+	values := reflect.ValueOf(model)
+
+	for i := 0; i < types.NumField(); i++ {
+		field := types.Field(i)
+		value := values.Field(i).Interface()
+		jsonTag := field.Tag.Get("json")
+		if jsonTag == "" {
+			jsonTag = field.Name
+		}
+		messageTag := field.Tag.Get("msg")
+		msg := ge.getErrorMessage(messageTag)
+
+		fmt.Printf("%s: %v = %v, tag= %v\n", field.Name, field.Type, value, jsonTag)
+		fields[field.Name] = ErrorResult{
+			Field:   field.Name,
+			JsonTag: jsonTag,
+			Message: msg,
+		}
+	}
+
+	for _, e := range err.(validator.ValidationErrors) {
+		if field, ok := fields[e.Field()]; ok {
+			if field.Message != "" {
+				errors[field.JsonTag] = field.Message
+			} else {
+				errors[field.JsonTag] = e.Error()
+			}
+		}
+	}
+
+	return errors
+}
+
+func (ge ginErrors) getErrorMessage(key string) string {
+	if value, ok := ge.errorMaps[key]; ok {
+		return value
+	}
+	return key
+}
---
+++ errors_test.go
@@ -0,0 +1,133 @@
+package ShyGinErrors
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/gin-gonic/gin"
+	. "github.com/onsi/ginkgo"
+	"github.com/stretchr/testify/assert"
+)
+
+// msg tag specific the key for the corresponse error
+type RegisterForm struct {
+	Email    string `json:"email" binding:"required,email" msg:"error_invalid_email"`
+	Username string `json:"username" binding:"required,alphanum,gte=6,lte=32" msg:"error_invalid_username"`
+	Password string `json:"password" binding:"required,gte=6,lte=32" msg:"error_invalid_password"`
+}
+
+var requestErrorMessage = map[string]string{
+	"error_invalid_email":    "请输入一个有效地meail地址",
+	"error_invalid_username": "username仅包含大小写字母和数字，长度6-32",
+	"error_invalid_password": "密码长度6-32",
+}
+
+func MockJsonPost(c *gin.Context, content interface{}) {
+	c.Request.Method = "POST"
+	c.Request.Header.Set("Content-Type", "application/json")
+
+	jsonbytes, err := json.Marshal(content)
+	if err != nil {
+		panic(err)
+	}
+
+	c.Request.Body = io.NopCloser(bytes.NewBuffer(jsonbytes))
+}
+
+var _ = Describe("test validator", func() {
+	var ctx *gin.Context
+	var ge GinErrors
+
+	BeforeEach(func() {
+		ge = NewShyGinErrors(requestErrorMessage)
+		w := httptest.NewRecorder()
+		ctx, _ = gin.CreateTestContext(w)
+		ctx.Request = &http.Request{
+			Header: make(http.Header),
+		}
+	})
+
+	It("test request validate username", func() {
+		MockJsonPost(ctx, map[string]interface{}{"username": "shyandsy!", "email": "shyandsy@gmail.com", "password": "123456"})
+
+		req := RegisterForm{}
+		if err := ctx.BindJSON(&req); err != nil {
+			log.Println(err.Error())
+			errors := ge.ListAllErrors(req, err)
+			assert.True(GinkgoT(), len(errors) == 1)
+
+			value, ok := errors["username"]
+			assert.True(GinkgoT(), ok)
+			msg, _ := requestErrorMessage["error_invalid_username"]
+			assert.True(GinkgoT(), value == msg)
+		} else {
+			assert.Fail(GinkgoT(), "error on username field has not been detected")
+		}
+	})
+
+	It("test request validate email", func() {
+		MockJsonPost(ctx, map[string]interface{}{"username": "shyandsy", "email": "shyandsygmail.com", "password": "123456"})
+
+		req := RegisterForm{}
+		if err := ctx.BindJSON(&req); err != nil {
+			errors := ge.ListAllErrors(req, err)
+			assert.True(GinkgoT(), len(errors) == 1)
+
+			value, ok := errors["email"]
+			assert.True(GinkgoT(), ok)
+			msg, _ := requestErrorMessage["error_invalid_email"]
+			assert.True(GinkgoT(), value == msg)
+		} else {
+			assert.Fail(GinkgoT(), "error on email field has not been detected")
+		}
+	})
+
+	It("test request validate password", func() {
+		MockJsonPost(ctx, map[string]interface{}{"username": "shyandsy", "email": "shyandsy@gmail.com", "password": "12345"})
+
+		req := RegisterForm{}
+		if err := ctx.BindJSON(&req); err != nil {
+			errors := ge.ListAllErrors(req, err)
+			assert.True(GinkgoT(), len(errors) == 1)
+
+			value, ok := errors["password"]
+			assert.True(GinkgoT(), ok)
+			msg, _ := requestErrorMessage["error_invalid_password"]
+			assert.True(GinkgoT(), value == msg)
+		} else {
+			assert.Fail(GinkgoT(), "error on email field has not been detected")
+		}
+	})
+
+	It("test request validate username, email, password", func() {
+		MockJsonPost(ctx, map[string]interface{}{"username": "shyandsy!", "email": "shyandsygmail.com", "password": "12345"})
+
+		req := RegisterForm{}
+		if err := ctx.BindJSON(&req); err != nil {
+			errors := ge.ListAllErrors(req, err)
+			assert.True(GinkgoT(), len(errors) == 3)
+
+			value, ok := errors["username"]
+			assert.True(GinkgoT(), ok)
+			msg, _ := requestErrorMessage["error_invalid_username"]
+			assert.True(GinkgoT(), value == msg)
+
+			value, ok = errors["email"]
+			assert.True(GinkgoT(), ok)
+			msg, _ = requestErrorMessage["error_invalid_email"]
+			assert.True(GinkgoT(), value == msg)
+
+			value, ok = errors["password"]
+			assert.True(GinkgoT(), ok)
+			msg, _ = requestErrorMessage["error_invalid_password"]
+			assert.True(GinkgoT(), value == msg)
+
+		} else {
+			assert.Fail(GinkgoT(), "error on username, email, password fields has not been detected")
+		}
+	})
+})
